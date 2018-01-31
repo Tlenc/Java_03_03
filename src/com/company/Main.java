c5b19d737aae7bfdd90b98d3e8d29d5e869a9214
Tlenc <tlenciauskas@gmail.com>
2018-01-31 12:08:45 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        float suma = 0;
+        float sk = 0;
+
+        do {
+            System.out.println("Įveskite skaičių");
+            Scanner sc = new Scanner(System.in);
+            sk = sc.nextFloat();
+            suma += sk;
+        }
+        while (sk != 0);
+
+        System.out.println("Suma : " + suma);
+
     }
+
+
 }
+
+
